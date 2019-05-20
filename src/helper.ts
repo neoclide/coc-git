07602214d5e9de73f9888f9c9098491af7bc3f36
David Mejorado <david.mejorado@gmail.com>
2019-05-19 18:22:31 -0700

    Add block support for git.browserOpen (#11)
    Read the command range to open the github URL with the selected lines.
    
    Depends on coc.nvim#789

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,6 @@
 import { Uri, workspace } from 'coc.nvim'
 
-export function getUrl(remote: string, branch: string, filepath: string, line?: number): string {
+export function getUrl(remote: string, branch: string, filepath: string, lines?: number[]): string {
   let uri = remote.replace(/\.git$/, '')
   if (uri.startsWith('git')) {
     let str = uri.slice(4)
@@ -9,7 +9,8 @@ export function getUrl(remote: string, branch: string, filepath: string, line?: 
   }
   let u = Uri.parse(uri)
   if (u.authority.startsWith('github.com')) {
-    return uri + '/blob/' + branch + '/' + filepath + (line ? '#L' + line : '')
+    const anchor = lines ? lines.map(l => `L${l}`).join('-') : ''
+    return uri + '/blob/' + branch + '/' + filepath + (anchor ? '#' + anchor : '')
   }
   workspace.showMessage(`Can't get url form: ${u.authority}`)
   return ''
