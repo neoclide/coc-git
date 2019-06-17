a0e70aed59ae49f6108f8d77e82c1f39c4c617f4
chemzqm <chemzqm@gmail.com>
2019-06-18 01:19:20 +0800

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -13,5 +13,4 @@ mkdir -p .release/lib/server
 cp -r Readme.md .release
 cat package.json | json -e 'this.dependencies={};this.scripts={}' > .release/package.json
 webpack --config webpack.config.js
-webpack --config webpack.server.config.js
 cd .release && npm publish
