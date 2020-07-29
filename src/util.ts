a1988c7aad754928de1007332a83b5778bf82b77
Weirong Xu <weirongxu.raidou@gmail.com>
2020-07-29 03:31:10 -0700

    fix indent and other platform

--- src/util.ts
+++ src/util.ts
@@ -23,8 +23,9 @@ export function shellescape(s: string): string {
 
 export function toUnixSlash(fsPath: string): string {
   if (process.platform == 'win32') {
-      return fsPath.replace(/\\/g, '/')
+    return fsPath.replace(/\\/g, '/')
   }
+  return fsPath
 }
 
 export async function safeRun(cmd: string, opts: ExecOptions = {}): Promise<string> {
