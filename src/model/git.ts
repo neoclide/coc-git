118e5cab21d60ed94e9f21978b71560302f97c4a
lsq <lsqypj.gmail.com>
2024-12-19 19:01:42 +0800

    fix issue #217, Support cgywin/msys2 bash environment

--- src/model/git.ts
+++ src/model/git.ts
@@ -83,7 +83,9 @@ export default class Git {
     options.env = Object.assign({}, process.env, options.env || {}, {
       LC_ALL: 'en_US.UTF-8',
       LANG: 'en_US.UTF-8'
-    })
+   })
+
+    options.shell = options.env.SHELL? options.env.SHELL : (process.platform === 'win32' || !!options.shell);
 
     if (options.log !== false) {
       this.log(`> git ${args.join(' ')}\n`)
